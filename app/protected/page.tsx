0d9662d5c43c31ea768a48e594268005f721c337
Brian Toone <brtoone@samford.edu>
2026-09-22 14:43:58 -0500

    surrounded tag with suspense

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -42,7 +42,9 @@ export default function ProtectedPage() {
       <div className="w-full">
         <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
           <InfoIcon size="16" strokeWidth={2} />
-          <Sales />
+          <Suspense>
+           <Sales />
+          </Suspense>
         </div>
       </div>
       <div className="w-full">
